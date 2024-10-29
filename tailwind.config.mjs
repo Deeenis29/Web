0fb7ebf58cb345119bacc999237453c8585e1ad1
Denis H-c <denishc0404@gmail.com>
2024-10-28 23:20:40 -0500

    Avance-3

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,12 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			fontFamily: {
+				'nunito': ['Nunito', 'sans-serif'],
+				'montserrat': ['Montserrat', 'sans-serif'],
+			  },
+		},
 	},
 	plugins: [],
 }
